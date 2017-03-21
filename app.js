e3dca9410dab63bbda54107f14b4b5e487f00f5b
Rishav Agarwal <rishav.159@gmail.com>
2017-03-22 00:44:48 +0530

    more ex

--- app.js
+++ app.js
@@ -3,31 +3,19 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express');
 var app = module.exports = express.createServer();
-var path = require('path');
 // Configuration
 
-app.configure(function(){
   app.use(express.bodyParser());
-  app.use(express.methodOverride());
 
   app.use(express.static(__dirname + '/public'));
-  });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
 
 // Routes
 
-app.get('/', routes.index);
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+app.listen(3070, function(){
+  console.log("listening on 3070");
 });
